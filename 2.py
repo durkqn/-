93987ddedecc635973d668ec2887f185dde3b2f2
durkqn <163218414+durkqn@users.noreply.github.com>
2024-05-22 11:43:10 +0900

    hangman1312312312

--- 2.py
+++ 2.py
@@ -1,59 +1,119 @@
-import pygame, math
-#1. 게임 초기화
+import pygame, math, random
+
+# 1. 게임 초기화
 pygame.init()
-#2. 게임창 옵션
-size=[500,900]
-screen=pygame.display.set_mode(size)
-title="HANGMAN"
+# 2. 게임창 옵션 설정
+size = [500,900]
+screen = pygame.display.set_mode(size)
+title = "HANGMAN"
 pygame.display.set_caption(title)
-#3. 게임내 필요한 설정
-clock=pygame.time.Clock()
-black=(0,0,0)
-white=(255,255,255)
+# 3. 게임 내 필요한 설정
+clock = pygame.time.Clock()
+black = (0,0,0,)
+white = (255,255,255)
+red = (255,0,0)
+hint_font = pygame.font.Font("C:/Windows/Fonts/arial.ttf", 80)
+entry_font = pygame.font.Font("C:/Windows/Fonts/arial.ttf", 60)
+no_font = pygame.font.Font("C:/Windows/Fonts/arial.ttf", 40)
 def tup_r(tup):
-    temp_list=[]
+    temp_list = {}
     for a in tup:
         temp_list.append(round(a))
     return tuple(temp_list)
-exit=False
-#4. 메인 이벤트
+entry_text = ""
+drop = False
+enter_go = False
+exit = False
+
+# A가 영어 단어를 1개 생각한다.
+f = open("voca.txt", "r", encoding='UTF-8')
+raw_data = f.read()
+f.close(0)
+data_list = raw_data.split("\n")
+data_list = data_list[:-1]
+while True:
+    r_index = random.randrange(0,len(data_list))
+    word = data_list[r_index].replace(u"\xa0", u"").split("")[1]
+    if len(word) <=6 : break
+word = word.upper()
+
+# 단어의 글자 수만큼 밑줄을 긋는다.
+word_show = "_"*len(word)
+try_num = 0
+ok_list = []
+no_list = []
+
+k = 0
+# 4. 메인 이벤트
 while not exit:
-#4-1. FPS 설정
+    # 4-1. FPS 설정
     clock.tick(60)
-#4-2. 각종입력감지
+    # 4-2. 각종 입력 감지
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
-#4-3. 입력, 시간에 따른 변화
-#4-4. 그리기
+        if event.type == pygame.KEYDOWN:
+            key_name = pygame.key.name(event.key)
+            if (key_name == "return" or key_name == "enter"):
+                if entry_text != "" and (ok_list+no_list).count(entry_text) == 0 :
+                    enter_go = True
+            elif len(key_name) == 1:
+                if (ord(key_name) >= 65 and ord(key_name) <= 90) or (ord(key_name) >= 97 and ord(key_name) <= 122):
+                    entry_text = key_name.upper()
+                else : entry_text = ""
+            else : entry_text = ""
+
+    # 4-3. 입력, 시간에 따른 변화
+    if try_num == 8 : k += 1
+    if enter_go == True:
+        ans = entry_text
+        rsult = word.find(ans)
+        if result == -1: #없음
+            try_num +=1
+            no_list.append(ans)
+        else : #있음
+            ok_list.append(ans)
+            for i in range(len(word)):
+                if word[i] == ans:
+                    word_show = word_show[:i] + ans + word_show[i+1:]
+        enter_go = False
+        entry_text = ""
+    # 4-4. 그리기
     screen.fill(black)
-    A=tup_r((0,size[1]*2/3))
-    B=(size[0], size[1]*2/3)
-    C=tup_r((size[0]/6, A[1]))
-    D=(C[0],C[0])
-    E=tup_r((size[0]/2,D[1]))
+    A = tup_r((0, size[1]*2/3))
+    B = (size[0], A[1])
+    C = tup_r((size[0]/6, A[1]))
+    D = (C[0], C[0])
+    E = tup_r((size[0]/2, D[1]))
     pygame.draw.line(screen, white, A, B, 3)
     pygame.draw.line(screen, white, C, D, 3)
     pygame.draw.line(screen, white, D, E, 3)
-    F=tup_r((E[0],E[1]+size[0]/6))
-    pygame.draw.line(screen, white, E, F, 3)
-    r_head=round(size[0]/12)
-    G=(F[0],F[1]+r_head)
-    pygame.draw.line(screen, white, G, r_head, 3)
-    H=(G[0],G[1]+r_head)
-    I=(G[0],H[1]+r_head)
-    pygame.draw.line(screen, white, H, I, 3)
-    I_arm=r_head*2
-    J=(I[0]-I_arm*math.cos(30*math.pi/180),
-       I[1]+I_arm*math.sin(30*math.pi/180))
-    J=tup_r(J)
-    pygame.draw.line(screen, white, I, J, 3)
-    K=(I[0]+I_arm*math.cos(30*math.pi/180),J[1])
-    pygame.draw.line(screen, white, I, K, 3)
-    L=(I[0],I[1]+I_arm)
-    pygame.draw.line(screen, white, I, L, 3)
+    F = tup_r((E[0], E[1]+size[0]/6))
+    if drop == False:
+        pygame.draw.line(screen, white, E, F, 3)
+    r_head = round(size[0]/12)
+    if drop == True : G = (F[0], F[1]+r_head+k*10)
+    else : G = (f[0], F[1]+r_head)
+    if try_num >= 1 : pygame.draw.circle(screen, white, G, r_head, 3)
+    H = (G[0], G[1]+r_head)
+    I = (H[0], H[1]+r_head)
+    if try_num >= 2 :pygame.draw.line(screen, white, H, I, 3)
+    l_arm = r_head*2
+    J = (I[0]-l_arm*math.cos(30*math.pi/180),
+         I[1]+l_arm*math.sin(30*math.pi/180))
+    K = (I[0]+l_arm*math.cos(30*math.pi/180),
+         I[0]+l_arm*math.sin(30*math.pi/180))
+    J = tup_r(J)
+    K = tup_r(K)
+    if try_num >= 3 :pygame.draw.line(screen, white, I, K, 3)
+    if try_num >= 4 :pygame.draw.line(screen, white, I, K, 3)
+    L = (I[0], I[1]+l_arm)
+    if try_num >= 5 :pygame.draw.line(screen, white, I, L, 3)
+    l_leg = round(l_arm * 1.5)
+    M = (L[0]-l_arm*math.cos(60*math.pi/180),
+         L[1]+l_arm*math.sin(60*math.pi/180))
+    N = (L[0]+l_arm*math.cos(60*math.pi/180),
+         L[1]+l_arm*math.sin(60*math.pi/180))
+    M = tup_r(M)
+    N = tup_r(N)
     
-#4-5. 업데이트
-    pygame.display.flip()
-#5. 게임종료
-pygame.quit()
